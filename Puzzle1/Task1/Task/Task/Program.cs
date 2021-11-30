d64493256fe22e74ada20f78905bd1786889e59a
Mate Sosa <mate.sosa@pakel.hr>
2021-11-30 21:54:35 +0100

    Template Program

---
+++ Puzzle1/Task1/Task/Task/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var lines = File.ReadAllLines("input1.txt");
+           
+        }
+    }
+}
