6c7ae448c1756f0040e00a3dc536fcc66d336437
Mate Sosa <mate.sosa@pakel.hr>
2021-12-01 13:40:04 +0100

    Small improvments

--- Puzzle1/Task1/Task/Task/Program.cs
+++ Puzzle1/Task1/Task/Task/Program.cs
@@ -9,26 +9,22 @@ namespace Task
         static void Main(string[] args)
         {
             var lines = File.ReadAllLines("input.txt");
-
             var counter = 0;
-
-
 
             for (int i = 0; i < lines.Length; i++)
             {
-                if (i > 0)
-                {
-                    if (int.Parse(lines[i - 1]) < int.Parse(lines[i]))
-                        counter++;
-                
-                }
+                if (i + 1 > lines.Length - 1)
+                    break;
 
+                var iznosi = int.Parse(lines[i]);
+                var iznosi1 = int.Parse(lines[i + 1]);
+
+                if (iznosi1 > iznosi)
+                    counter++;
             }
-
 
             Console.WriteLine("Result is {0}", counter);
             Console.ReadKey();
-
         }
     }
 }
--- Puzzle1/Task2/Task/Task/Program.cs
+++ Puzzle1/Task2/Task/Task/Program.cs
@@ -9,11 +9,8 @@ namespace Task
         static void Main(string[] args)
         {
             var lines = File.ReadAllLines("input.txt");
-
             var counter = 0;
-
-
-
+            
             for (int i = 0; i < lines.Length; i++)
             {
                 var i1 = i;
@@ -27,14 +24,9 @@ namespace Task
                 var sum1 = int.Parse(lines[i1]) + int.Parse(lines[i2]) + int.Parse(lines[i3]);
                 var sum2 = int.Parse(lines[i2]) + int.Parse(lines[i3]) + int.Parse(lines[i4]);
 
-                if (sum2 > sum1)
-                {
-                        counter++;
-                
-                }
-
+                if (sum2 > sum1) counter++;
+            
             }
-
 
             Console.WriteLine("Result is {0}", counter);
             Console.ReadKey();
