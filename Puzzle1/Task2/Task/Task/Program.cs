e97c6f6549fe5d70c2a4109cfa924180065671ad
Mate Sosa <mate.sosa@pakel.hr>
2021-12-01 07:28:18 +0100

    Templaate progra

--- Puzzle1/Task2/Task/Task/Program.cs
+++ Puzzle1/Task2/Task/Task/Program.cs
@@ -16,9 +16,19 @@ namespace Task
 
             for (int i = 0; i < lines.Length; i++)
             {
-                if (i > 0)
+                var i1 = i;
+                var i2 = i+1;
+                var i3 = i+2;
+                var i4 = i+3;
+
+                if (i4 > lines.Length - 1)
+                    break;
+
+                var sum1 = int.Parse(lines[i1]) + int.Parse(lines[i2]) + int.Parse(lines[i3]);
+                var sum2 = int.Parse(lines[i2]) + int.Parse(lines[i3]) + int.Parse(lines[i4]);
+
+                if (sum2 > sum1)
                 {
-                    if (int.Parse(lines[i - 1]) < int.Parse(lines[i]))
                         counter++;
                 
                 }
