a9fba27155ebe5d5ee8b92e56bc5978fb6d901ff
Mate Sosa <mate.sosa@pakel.hr>
2021-12-02 06:55:12 +0100

    Puzzle2 commited

---
+++ Puzzle2/Task1/Task/Task/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Task
+{
+    class Program
+    {
+        private static int _x = 0;
+        private static int _y = 0;
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+         
+            foreach (var line in lines)
+                ParseLine(line);
+
+            Console.WriteLine("Result is forward {0}, depth {1}, total {2} ", _x, _y, _x * _y);
+            Console.ReadKey();
+        }
+
+        public static void ParseLine(String line)
+        {
+            var splitedLine = line.Split(' ');
+
+            if (splitedLine[0] == "forward")
+                _x += int.Parse(splitedLine[1]);
+
+            if (splitedLine[0] == "down")
+                _y += int.Parse(splitedLine[1]);
+
+            if (splitedLine[0] == "up")
+                _y -= int.Parse(splitedLine[1]);
+
+        }
+    }
+}
---
+++ Puzzle2/Task2/Task/Task/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Task
+{
+    class Program
+    {
+        private static long _x = 0;
+        private static long _y = 0;
+        private static long _z = 0;
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            foreach (var line in lines)
+                ParseLine(line);
+
+            Console.WriteLine("Result is forward {0}, depth {1}, total {2} ", _x, _y, _x * _y);
+            Console.ReadKey();
+        }
+
+
+        public static void ParseLine(String line)
+        {
+            var splitedLine = line.Split(' ');
+
+            if (splitedLine[0] == "forward")
+            {
+                _x += int.Parse(splitedLine[1]);
+                _y += _z * int.Parse(splitedLine[1]);
+            }
+
+            if (splitedLine[0] == "down")
+                _z += int.Parse(splitedLine[1]);
+
+            if (splitedLine[0] == "up")
+                _z -= int.Parse(splitedLine[1]);
+
+        }
+    }
+}
