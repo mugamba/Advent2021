0296c5d1a5f2c8882962389f48898bb102ebb116
Mate Sosa <mate.sosa@pakel.hr>
2021-12-05 07:49:11 +0100

    Puzlle 5 drawing lines

---
+++ Puzzle5/Task1/Task/Task/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Task
+{
+    class Program
+    {
+        public static List<Tuple<Point, Point>> _coordinatePairs = new List<Tuple<Point, Point>>();
+        public static int[,] _plotinglinesField = new int[1000, 1000];
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            foreach (var line in lines)
+                _coordinatePairs.Add(ParsePair(line));
+
+            //only horizontal and vertical
+            var filteredCoordinates = _coordinatePairs.Where(o => o.Item1.X == o.Item2.X || o.Item1.Y == o.Item2.Y).ToList();
+            PlotLines(filteredCoordinates);
+
+            Console.Out.WriteLine("Result is {0}", CountOverlapinField());
+            Console.ReadKey();
+
+
+
+        }
+
+
+        private static Tuple<Point, Point> ParsePair(String line)
+        {
+            var linesplited = line.Split("->");
+            var cOne = linesplited[0];
+            var cTwo = linesplited[1];
+
+            var cOneSplited = cOne.Split(",");
+            var cTwoSplited = cTwo.Split(",");
+
+            var pointOne = new Point(int.Parse(cOneSplited[0]), int.Parse(cOneSplited[1]));
+            var pointTwo = new Point(int.Parse(cTwoSplited[0]), int.Parse(cTwoSplited[1]));
+
+            return new Tuple<Point, Point>(pointOne, pointTwo);
+
+        }
+
+        private static void PlotLines(List<Tuple<Point, Point>> pairsToPlot)
+        {
+
+            foreach (var pair in pairsToPlot )
+            {
+                var pairFromX = pair.Item1.X < pair.Item2.X ? pair.Item1.X : pair.Item2.X;
+                var pairFromY = pair.Item1.Y < pair.Item2.Y ? pair.Item1.Y : pair.Item2.Y;
+                var pairToX = pair.Item1.X > pair.Item2.X ? pair.Item1.X : pair.Item2.X;
+                var pairToY = pair.Item1.Y > pair.Item2.Y ? pair.Item1.Y : pair.Item2.Y;
+                for (int i = pairFromX; i < pairToX + 1; i++)
+                    for (int j = pairFromY; j < pairToY + 1; j++)
+                        _plotinglinesField[i, j]++;
+
+            }
+
+           
+        }
+
+        private static int CountOverlapinField()
+        {
+            var counter = 0;
+            for (int i = 0; i < 1000; i++)
+                for (int j = 0; j < 1000; j++)
+                    if (_plotinglinesField[i, j] >= 2 )
+                        counter++;
+
+
+            return counter;
+
+        }
+
+
+    }
+}
