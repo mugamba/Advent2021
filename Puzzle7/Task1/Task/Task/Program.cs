6b2c51447a456af10be30dae99c3bf422bd40f20
Mate Sosa <mate.sosa@pakel.hr>
2021-12-07 07:09:48 +0100

    Craba alignin submarines

---
+++ Puzzle7/Task1/Task/Task/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Task
+{
+    class Program
+    {
+
+        public static List<int> _list = new List<int>();
+        public static List<long> _sums = new List<long>();
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt").First();
+            var splitted = lines.Split(",");
+            _list.AddRange(splitted.Select(o => int.Parse(o)).ToArray());
+
+
+            var max = _list.Max();
+            var min = _list.Min();
+
+            for (int i = min; i < max + 1; i++)
+            {
+                _sums.Add(SumPathsToNumber(i));
+
+            }
+
+            Console.WriteLine("Result is {0}", _sums.Min());
+            Console.ReadKey();
+        }
+
+        private static long SumPathsToNumber(Int32 number)
+        {
+            long sum = 0;
+            for (int i = 0; i < _list.Count; i++)
+            {
+                sum += Math.Abs(number-_list[i]);
+
+            }
+            return sum;
+        
+        }
+    }
+}
