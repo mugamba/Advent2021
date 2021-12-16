3edcd751fc4d7dfcc8a90996683633b669de67e7
Mate Sosa <mate.sosa@pakel.hr>
2021-12-16 21:16:16 +0100

    deconding string

--- Puzzle16/Task1/Task/Task/Program.cs
+++ Puzzle16/Task1/Task/Task/Program.cs
@@ -10,37 +10,48 @@ namespace Task
     class Program
     {
 
-        public static int versionSum = 0;
+        public static int _versionSum = 0;
+        public static IList<long> _literals = new List<long>();
         static void Main(string[] args)
         {
-            var lines = File.ReadAllLines("input.txt");
+            var lines = File.ReadAllLines("input.txt").First();
 
-            string binarystring = String.Join(String.Empty,
-              "8A004A801A8002F478".Select(
+            string binarystring = String.Join(String.Empty,lines
+             .Select(
                 c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')
               )
 );
 
             ParsePacket(binarystring);
 
+            
             Console.WriteLine("Result is {0}", binarystring);
             Console.ReadKey();
         }
 
 
-        private static void ParsePacket(String packet)
+        private static String ParsePacket(String packet)
         {
+            if (packet == "")
+                return "";
+
+            if (packet.Length < 8 && packet.ToCharArray().All(o => o == '0'))
+                return "";
+
             var version = packet.Substring(0, 3);
             var versionDec =Convert.ToInt32(version, 2);
 
-            versionSum += versionDec;
+            _versionSum += versionDec;
 
             var type = packet.Substring(3, 3);
             var typeDec = Convert.ToInt32(type, 2);
 
             if (typeDec == 4)
             {
+                var input = packet.Substring(6, packet.Length - 6);
 
+                input = ParseLitreal(input);
+                return ParsePacket(input);
             }
             else
             {
@@ -54,24 +65,54 @@ namespace Task
                     var l = packet.Substring(7, 15);
                     ldecimal = Convert.ToInt32(l, 2);
                     subpackets = packet.Substring(22, packet.Length - 22);
-
+                    string rest = "";
+                    while (ldecimal > 0)
+                    {
+                        var bbefore = subpackets.Length;
+                        rest = ParsePacket(subpackets);
+                        ldecimal = ldecimal - (bbefore - rest.Length);
+                    }
+                    return ParsePacket(rest);
                 }
-                else {
+                else
+                {
                     var l = packet.Substring(7, 11);
                     numofsegments = Convert.ToInt32(l, 2);
                     subpackets = packet.Substring(18, packet.Length - 18);
+                    string rest = subpackets;
+                    for (int i = 0; i < numofsegments; i++)
+                    {
+                        rest = ParsePacket(rest);
+                    
+                    }
+
                 }
-
-                if (numofsegments == 0)
-
-
-                
 
             }
 
+            return "";
 
          
         }
 
+        private static string ParseLitreal(string input)
+        {
+            var literals = new StringBuilder();
+
+            while (true)
+            {
+
+                var literal = input.Substring(0, 5);
+                literals.Append(literal.Substring(1, 4));
+                input = input.Substring(5, input.Length-5);
+
+                if (literal.StartsWith("0"))
+                    break;
+            }
+
+            _literals.Add(Convert.ToInt64(literals.ToString(), 2));
+
+            return input;
+        }
     }
 }
