445c522b8a133980554915d6d96a42113cf7037e
Mate Sosa <mate.sosa@pakel.hr>
2021-12-24 12:53:55 +0100

    Gittasks

--- Puzzle22/Task1/Task/Task/Program.cs
+++ Puzzle22/Task1/Task/Task/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,102 +10,151 @@ namespace Task
 {
     class Program
     {
-        public static List<Cuboid> _cuboids = new List<Cuboid>();
-        public static Cuboid _totalCubo;
+
+
 
         static void Main(string[] args)
         {
-            var lines = File.ReadAllLines("input.txt");
 
-            foreach (var line in lines)
+            var puzz = new Puzzle22();
+            puzz.Setup();
+            Console.WriteLine(puzz.SolvePart2());
+            Console.ReadLine();
+
+        }
+
+    }
+
+    public class Puzzle22
+    {
+        private List<Cube> cubes;
+
+        public void Setup()
+        {
+            List<string> items = File.ReadAllLines("input.txt").ToList();
+            cubes = new List<Cube>();
+            for (int i = 0; i < items.Count; i++)
             {
+                string line = items[i];
+                cubes.Add(new Cube(line));
+            }
+        }
 
-                var cubo = new Cuboid();
-                var splits = line.Split(" ");
-                cubo.opp = splits[0];
-                var nextssplits = splits[1].Split(",");
-                var stringx = nextssplits[0].Replace("x=", "");
-                var splitx = stringx.Split("..");
-                cubo.leftx = int.Parse(splitx[0]);
-                cubo.rightx = int.Parse(splitx[1]);
-                var stringy =  nextssplits[1].Replace("y=", "");
-                var splity = stringy.Split("..");
-                cubo.lefty = int.Parse(splity[0]);
-                cubo.righty = int.Parse(splity[1]);
-                var stringz =  nextssplits[2].Replace("z=", "");
-                var splitz = stringz.Split("..");
-                cubo.leftz = int.Parse(splitz[0]);
-                cubo.rightz = int.Parse(splitz[1]);
+        [Description("How many cubes are on?")]
+        public string SolvePart1()
+        {
+            List<Cube> calculated = new List<Cube>();
+            long totalVolume = 0;
+            for (int i = cubes.Count - 1; i >= 0; i--)
+            {
+                Cube cube = cubes[i];
+                if (!cube.IsValid(-50, 50)) { continue; }
+
+                if (cube.Value)
+                {
+                    totalVolume += cube.Volume() - Overlap(calculated, cube, 0);
+                }
+                calculated.Add(cube);
+            }
+            return $"{totalVolume}";
+        }
+
+        [Description("How many cubes are on for the whole input?")]
+        public string SolvePart2()
+        {
+            List<Cube> calculated = new List<Cube>();
+            long totalVolume = 0;
+            for (int i = cubes.Count - 1; i >= 0; i--)
+            {
+                Cube cube = cubes[i];
+
+                if (cube.Value)
+                {
+                    totalVolume += cube.Volume() - Overlap(calculated, cube, 0);
+                }
+                calculated.Add(cube);
+            }
+            return $"{totalVolume}";
+        }
+        private long Overlap(List<Cube> calculated, Cube cube, int index)
+        {
+            long volume = 0;
+            for (int i = index; i < calculated.Count; i++)
+            {
+                Cube other = calculated[i];
+                if (!cube.Overlaps(other)) { continue; }
+
+                Cube overlap = cube.Intersection(other);
+                volume += overlap.Volume() - Overlap(calculated, overlap, i + 1);
+            }
+            return volume;
+        }
+        private class Cube
+        {
+            public int X1, X2;
+            public int Y1, Y2;
+            public int Z1, Z2;
+            public bool Value;
+
+            public Cube(int x1, int x2, int y1, int y2, int z1, int z2, bool value)
+            {
+                X1 = x1; X2 = x2;
+                Y1 = y1; Y2 = y2;
+                Z1 = z1; Z2 = z2;
+                Value = value;
+            }
+            public Cube(string line)
+            {
+                var tt = line.Split(" ");
+                var xyz = tt[1].Split(",");
+
+                var x = xyz[0].Split("..");
+                var y = xyz[1].Split("..");
+                var z = xyz[2].Split("..");
 
 
-                _cuboids.Add(cubo);
+                // string[] splits = Tools.SplitOn(line, " x=", "..", ",y=", "..", ",z=", "..");
+                Value = tt[0] == "on";
+                X1 = int.Parse(x[0].Replace("x=", ""));
+                X2 = int.Parse(x[1].Replace("x=", ""));
+
+                Y1 = int.Parse(y[0].Replace("y=", ""));
+                Y2 = int.Parse(y[1].Replace("y=", ""));
+
+                Z1 = int.Parse(z[0].Replace("z=", ""));
+                Z2 = int.Parse(z[1].Replace("z=", ""));
 
             }
-
-
-
-
-          
-
-            //on x = -18..26, y = -33..15, z = -7..46
-            //off x = -40..-22, y = -38..-28, z = 23..41
-            //-18..26 -27..15 -7..22 
-
-
-            Console.WriteLine("Result is {0}", _totalCubo.PointsInCube());
+            public bool IsValid(int min, int max)
+            {
+                return X1 <= max && X2 >= min
+                    && Y1 <= max && Y2 >= min
+                    && Z1 <= max && Z2 >= min;
+            }
+            public long Volume()
+            {
+                return (long)(X2 - X1 + 1) * (Y2 - Y1 + 1) * (Z2 - Z1 + 1);
+            }
+            public bool Overlaps(Cube cube)
+            {
+                return X1 <= cube.X2 && X2 >= cube.X1
+                    && Y1 <= cube.Y2 && Y2 >= cube.Y1
+                    && Z1 <= cube.Z2 && Z2 >= cube.Z1;
+            }
+            public Cube Intersection(Cube cube)
+            {
+                return new Cube(Math.Max(X1, cube.X1), Math.Min(X2, cube.X2),
+                Math.Max(Y1, cube.Y1), Math.Min(Y2, cube.Y2),
+                Math.Max(Z1, cube.Z1), Math.Min(Z2, cube.Z2), true);
+            }
+            public override string ToString()
+            {
+                return $"({X1}..{X2})({Y1}..{Y2})({Z1}..{Z2}) On={Value}";
+            }
         }
-
-       
-          
-
-            
-            Console.WriteLine("Result {0}", ret.ToString());
-            return ret;
-        }
-
-
-
-
-
-        public static IList<Cuboid> OperateOnX(Cuboid input, Cuboid next)
-        { 
-            if (input.leftx >= next.leftx && input.rightx
-                
-                
-                )
-
-        
-        }
-
     }
-
-    public class Cuboid
-    {
-        public string opp;
-        public int leftx;
-        public int rightx;
-        public int lefty;
-        public int righty;
-        public int leftz;
-        public int rightz;
-
-
-        public string ToString()
-        {
-            return String.Format("x={0}..{1}, y={2}..{3}, z={4}..{5}", leftx, rightx, lefty, righty, leftz, rightz);
-        
-        }
-
-        public long PointsInCube()
-        {
-            return Math.Abs(leftx - rightx) * Math.Abs(lefty - righty) * Math.Abs(leftz - rightz);
-
-        }
-
-
-
-    }
-
-
-
 }
+
+
+
+
---
+++ Puzzle24/Task1/Task/Task/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Task
+{
+    class Program
+    {
+        public static Dictionary<string, int> _regiseters = new Dictionary<string, int>();
+        public static int[] _input = new int[14];
+        public static int _inputCount = 0;
+        public static List<Segment> _segments = new List<Segment>();
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            for (int s = 0; s < 14; s++)
+            {
+                var seg = new Segment();
+                seg._lines = lines.Skip(s * 18).Take(18).ToArray();
+                _segments.Add(seg);
+            }
+
+
+            for (int s = 14; s > 0; s--)
+            {
+              
+                var sLines = _segments[s-1]._lines;
+                var validRange = new List<int>();
+                validRange.Add(0);
+                if (s<14)
+                    validRange = _segments[s].combinationswZ.Select(o => o.Item1).ToList();
+
+
+
+               int leftz = 0; int rightz = 0;
+                if (s < 14)
+                {
+                    leftz = _segments[s].minz();
+                    rightz = _segments[s].maxz();
+                }
+
+                for (int z = 0; z < 10000; z++)
+                    for (int w = 0; w < 10; w++)
+                    {
+                        _regiseters["x"] = 0;
+                        _regiseters["y"] = 0;
+                        _regiseters["z"] = z;
+                        _input[0] = w;
+
+                        foreach (var ssLines in sLines)
+                        {
+                            ParseLine(ssLines);
+                        }
+
+                        if ( )
+                        {
+                            _segments[s-1].combinationswZ.Add(new Tuple<int, int>(z, w));
+                        }
+
+                    }
+            }
+            
+        
+
+            Console.WriteLine("Result is {0}", _input);
+
+        }
+
+
+        
+
+
+        public static void ParseLine(String line)
+        {
+
+            if (line.StartsWith("inp"))
+            {
+                _regiseters[line.Split(" ")[1]] = _input[_inputCount];
+            }
+
+            if (line.StartsWith("add"))
+            {
+                var value = 0;
+                var reg = line.Split(" ")[1];
+                var token = line.Split(" ")[2];
+                value = int.TryParse(token, out value) ? value : _regiseters[token];
+                _regiseters[reg] = _regiseters[reg] + value;
+            }
+
+            if (line.StartsWith("mul"))
+            {
+                var value = 0;
+                var reg = line.Split(" ")[1];
+                var token = line.Split(" ")[2];
+                value = int.TryParse(token, out value) ? value : _regiseters[token];
+                _regiseters[reg] = _regiseters[reg] * value;
+            }
+
+            if (line.StartsWith("mod"))
+            {
+                var value = 0;
+                var reg = line.Split(" ")[1];
+                var token = line.Split(" ")[2];
+                value = int.TryParse(token, out value) ? value : _regiseters[token];
+                _regiseters[reg] = _regiseters[reg] % value;
+            }
+
+            if (line.StartsWith("div"))
+            {
+                var value = 0;
+                var reg = line.Split(" ")[1];
+                var token = line.Split(" ")[2];
+                value = int.TryParse(token, out value) ? value : _regiseters[token];
+                _regiseters[reg] = _regiseters[reg] / value;
+            }
+
+            if (line.StartsWith("eql"))
+            {
+                var value = 0;
+                var reg = line.Split(" ")[1];
+                var token = line.Split(" ")[2];
+                value = int.TryParse(token, out value) ? value : _regiseters[token];
+                _regiseters[reg] = _regiseters[reg] == value ? 1 : 0;
+            }
+
+        }
+
+        public class Segment
+        {
+            public String[] _lines;
+            public List<Tuple<int, int>> combinationswZ = new List<Tuple<int, int>>();
+            public int minz()
+            {
+                return combinationswZ.Select(o => o.Item1).Min();
+            }
+
+            public int maxz()
+            {
+                return combinationswZ.Select(o => o.Item1).Max();
+            }
+        }
+
+
+
+    }
+
+
+
+}
